0e824d80c66d18319437195d06246953f679a72e
Jitendra Sharma <jitendra149.tech@gmail.com>
2025-05-19 18:42:17 +0530

    #commit  variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,20 @@
+const accountid = 10001
+let accountemail ="jeet@gmal.com"
+var accountPassword = "jeet123"
+accountCity = "Agra"
+let accoutnState
+
+//accountid =2 
+
+/*
+Not prefer  var keyword because scope issue facing if we use var
+*/
+
+console.log(accountid)
+
+ 
+ accountemail ="jeet@outlook.com"
+ accountPassword = "54321"
+ accountCity = "Noida"
+
+console.table([accountid,accountemail,accountPassword,accountCity,accoutnState])
--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,5 +1,10 @@
-console.log("Jitendra")
-
+//console.log("Jitendra")
+accountid = 10001
 accountemail ="jeet@gmal.com"
 accountPassword = "jeet123"
 accountCity = "Agra"
+
+console.log(accountemail)
+
+console.table([accountid,accountemail,accountPassword,accountCity])
+
